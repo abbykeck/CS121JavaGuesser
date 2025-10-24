94bc4369df1acd4df45dd86e47cb8653fe7e4e1f
abbykeck <abigailkeck2425@gmail.com>
2025-10-24 18:40:29 +0000

    finished implementing JavaGuesser class, added exception handling for converting user input from String to int

--- JavaGuesser.java
+++ JavaGuesser.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.lang.*;
 public class JavaGuesser {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
 		new JavaGuesser();
 	}
 
@@ -43,15 +42,60 @@ public class JavaGuesser {
 				if (Integer.parseInt(guess) == number) {
 					keepGoing = false;
 					System.out.println("got it!");
-				}
+				} else if (Integer.parseInt(guess) > number) {
+					System.out.println("too high...");
+					turns++;
+				} else {
+					System.out.println("too low...");
+					turns++;
+				} // end if
 			} catch (Exception e) {
-				System.out.println(e.getMessage());
+				System.out.println("Invalid input, please try again");
 			} // end try
 		} // end while
+		System.out.println();
+		if (turns < 7) {
+			System.out.println("Very good!");
+		} else if (turns == 7) {
+			System.out.println("Good, you got the number right in an average amount of turns.");
+		} else {
+			System.out.println("Can you get the number right with less turns?");
+		} // end if
+		System.out.println();
 	} // end humanGuesser
 
 	public void computerGuesser() {
+		Scanner input = new Scanner(System.in);
 		System.out.println("computer guesser");
+		int guess = 50;
+		int guessChange = 25;
+		int turns = 1;
+		String correct = "";
+		boolean keepGoing = true;
+		while (keepGoing) {
+			System.out.println(turns + ") I guess " + guess);
+			System.out.print("Too (H)igh, too (L)ow, or (C)orrect? ");
+			correct = input.nextLine();
+			if (correct.toUpperCase().equals("C")) {
+				keepGoing = false;
+			} else if (correct.toUpperCase().equals("L")) {
+				guess += guessChange;
+				guessChange = (int)Math.floor(guessChange / 2.0);
+				turns++;
+			} else if (correct.toUpperCase().equals("H")) {
+				guess -= guessChange;
+				guessChange = (int)Math.floor(guessChange / 2.0);
+				turns++;
+			} else {
+				System.out.println("Invalid input, please try again");
+			} // end if
+			
+			// to prevent the computer from getting stuck on a guess
+			if (guessChange == 0) {
+				guessChange = 1;
+			} // end if
+		} // end while
+		System.out.println();
 	} // end computerGuesser
 } // end JavaGuesser
 
